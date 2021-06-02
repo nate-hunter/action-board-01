b78461ca529d495ed89eab0a7297146fc82a2fcb
njh <nate.indemand@gmail.com>
2021-06-01 22:35:35 -0400

    simple apollo-server-express setup

--- server-actions-01/server.js
+++ server-actions-01/server.js
@@ -6,4 +6,37 @@ const dotEnv = require('dotenv');
 
 dotEnv.config();
 
-const app = 
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const typeDefs = gql`
+    type Query {
+        test: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        test: () => "Test query for a string"
+    }
+}
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', (req, res, next) => {
+    res.send({ message: 'TESTING EXPRESS SERVER...'})
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+})
